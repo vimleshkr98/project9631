62b9efd8a7548d513072e79fbe61dd2197e52ed1
vimleshkr98 <123216387+vimleshkr98@users.noreply.github.com>
2025-03-22 11:56:46 +0530

    Add files via upload

---
+++ JAVA/BASIC CODE/first.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+   int heroes=["heroes","vimlesh","mahto"];
+   printf(heroes[0]);
+
+    return 0;
+}
---
+++ JAVA/BASIC CODE/first.java
@@ -0,0 +1,6 @@
+class Firstclass{
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
